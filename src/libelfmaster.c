790fbdf47995643149f26ce3cbcf75c2ab063162
elfmaster <roneill@backtrace.io>
2017-05-22 15:27:16 -0700

    finished elf_reloc_type_string()

--- src/libelfmaster.c
+++ src/libelfmaster.c
@@ -81,10 +81,181 @@ const char *
 elf_reloc_type_string(struct elfobj *obj, uint32_t r_type)
 {
 
-	(void)obj;
-	(void)r_type;
+	/*
+	 * For now we only support printing relocations for x86 arch
+	 */
+	if (elf_machine(obj) != EM_386 && elf_machine(obj) != EM_X86_64)
+		return "R_UNKNOWN";
 
-	return "unknown";
+	switch(obj->e_class) {
+	case elfclass32:
+		switch(r_type) {
+		case R_386_NONE:
+			return "R_386_NONE";
+		case R_386_32:
+			return "R_386_32";
+		case R_386_PC32:
+			return "R_386_PC32";
+		case R_386_GOT32:
+			return "R_386_GOT32";
+		case R_386_PLT32:
+			return "R_386_PLT32";
+		case R_386_COPY:
+			return "R_386_COPY";
+		case R_386_GLOB_DAT:
+			return "R_386_GLOB_DAT";
+		case R_386_JMP_SLOT:
+			return "R_386_JUMP_SLOT";
+		case R_386_RELATIVE:
+			return "R_386_RELATIVE";
+		case R_386_GOTOFF:
+			return "R_386_GOTOFF";
+		case R_386_GOTPC:
+			return "R_386_GOTPC";
+		case R_386_32PLT:
+			return "R_386_32PLT";
+		case R_386_TLS_TPOFF:
+			return "R_386_TLS_TPOFF";
+		case R_386_TLS_LE:
+			return "R_386_TLS_LE";
+		case R_386_TLS_GD:
+			return "R_386_TLS_GD";
+		case R_386_TLS_LDM:
+			return "R_386_TLS_LDM";
+		case R_386_16:
+			return "R_386_16";
+		case R_386_PC16:
+			return "R_386_PC16";
+		case R_386_8:
+			return "R_386_8";
+		case R_386_PC8:
+			return "R_386_PC8";
+		case R_386_TLS_GD_32:
+			return "R_386_TLS_GD_32";
+		case R_386_TLS_GD_PUSH:
+			return "R_386_TLS_GD_PUSH";
+		case R_386_TLS_GD_CALL:
+			return "R_386_TLS_GD_CALL";
+		case R_386_TLS_GD_POP:
+			return "R_386_TLS_GD_POP";
+		case R_386_TLS_LDM_32:
+			return "R_386_TLS_LDM_32";
+		case R_386_TLS_LDM_PUSH:
+			return "R_386_TLS_LDM_PUSH";
+		case R_386_TLS_LDM_CALL:
+			return "R_386_TLS_LDM_CALL";
+		case R_386_TLS_LDM_POP:
+			return "R_386_TLS_LDM_POP";
+		case R_386_TLS_LDO_32:
+			return "R_386_TLS_LDO_32";
+		case R_386_TLS_IE_32:
+			return "R_386_TLS_IE_32";
+		case R_386_TLS_LE_32:
+			return "R_386_TLS_LE_32";
+		case R_386_TLS_DTPMOD32:
+			return "R_386_TLS_DTMOD32";
+		case R_386_TLS_DTPOFF32:
+			return "R_386_TLS_DTOFF32";
+		case R_386_TLS_TPOFF32:
+			return "R_386_TLS_TPOFF32";
+		case R_386_SIZE32:
+			return "R_386_TLS_SIZE32";
+		case R_386_TLS_GOTDESC:
+			return "R_386_TLS_GOTDESC";
+		case R_386_TLS_DESC_CALL:
+			return "R_386_TLS_DESC_CALL";
+		case R_386_TLS_DESC:
+			return "R_386_TLS_DESC";
+		case R_386_IRELATIVE:
+			return "R_386_IRELATIVE";
+		default:
+			return "UNKNOWN";
+		}
+	case elfclass64:
+		switch(r_type) {
+		case R_X86_64_NONE:
+			return "R_X86_64_NONE";
+		case R_X86_64_64:
+			return "R_X86_64_64";
+		case R_X86_64_PC32:
+			return "R_X86_64_PC32";
+		case R_X86_64_GOT32:
+			return "R_X86_64_GOT32";
+		case R_X86_64_PLT32:
+			return "R_X86_64_PLT32";
+		case R_X86_64_COPY:
+			return "R_X86_64_COPY";
+		case R_X86_64_GLOB_DAT:
+			return "R_X86_64_GLOB_DAT";
+		case R_X86_64_JUMP_SLOT:
+			return "R_X86_64_JUMP_SLOT";
+		case R_X86_64_RELATIVE:
+			return "R_X86_64_RELATIVE";
+		case R_X86_64_GOTPCREL:
+			return "R_X86_64_GOTPCREL";
+		case R_X86_64_32:
+			return "R_X86_64_32";
+		case R_X86_64_32S:
+			return "R_X86_64_32S";
+		case R_X86_64_16:
+			return "R_X86_64_16";
+		case R_X86_64_PC16:
+			return "R_X86_64_PC16";
+		case R_X86_64_8:
+			return "R_X86_64_8";
+		case R_X86_64_PC8:
+			return "R_X86_64_PC8";
+		case R_X86_64_DTPMOD64:
+			return "R_X86_64_DTPMOD64";
+		case R_X86_64_DTPOFF64:
+			return "R_X86_64_DTPOFF64";
+		case R_X86_64_TPOFF64:
+			return "R_X86_64_TPOFF64";
+		case R_X86_64_TLSGD:
+			return "R_X86_64_TLSGD";
+		case R_X86_64_TLSLD:
+			return "R_X86_64_TLSLD";
+		case R_X86_64_DTPOFF32:
+			return "R_X86_64_DTPOFF32";
+		case R_X86_64_GOTTPOFF:
+			return "R_X86_64_GOTTPOFF";
+		case R_X86_64_TPOFF32:
+			return "R_X86_64_TPOFF32";
+		case R_X86_64_PC64:
+			return "R_X86_64_PC64";
+		case R_X86_64_GOTOFF64:
+			return "R_X86_64_GOTOFF64";
+		case R_X86_64_GOTPC32:
+			return "R_X86_64_GOTPC32";
+		case R_X86_64_GOT64:
+			return "R_X86_64_GOT64";
+		case R_X86_64_GOTPCREL64:
+			return "R_X86_64_GOTPCREL64";
+		case R_X86_64_GOTPC64:
+			return "R_X86_64_GOTPC64";
+		case R_X86_64_GOTPLT64:
+			return "R_X86_64_GOTPLT64";
+		case R_X86_64_PLTOFF64:
+			return "R_X86_64_PLTOFF64";
+		case R_X86_64_SIZE32:
+			return "R_X86_64_SIZE32";
+		case R_X86_64_SIZE64:
+			return "R_X86_64_SIZE64";
+		case R_X86_64_GOTPC32_TLSDESC:
+			return "R_X86_64_GOTPC32_TLSDESC";
+		case R_X86_64_TLSDESC_CALL:
+			return "R_X86_64_TLSDESC_CALL";
+		case R_X86_64_TLSDESC:
+			return "R_X86_64_TLSDESC";
+		case R_X86_64_IRELATIVE:
+			return "R_X86_64_IRELATIVE";
+		case R_X86_64_RELATIVE64:
+			return "R_X86_64_RELATIVE64";
+		default:
+			return "R_UNKNOWN";
+		}
+	}
+	return "R_UNKNOWN";
 }
 
 uint64_t
@@ -1124,7 +1295,8 @@ elf_pltgot_iterator_next(struct elf_pltgot_iterator *iter, struct elf_pltgot_ent
 		entry->value = ptr[iter->index];
 	}
 	if (elf_section_by_name(iter->obj, ".plt", &section) == true) {
-		if (entry->value >= section.address && entry->value < section.address + section.size)
+		if (entry->value >= section.address &&
+		    entry->value < section.address + section.size)
 			entry->flags = ELF_PLTGOT_PLT_STUB_F;
 	}
 	entry->offset = iter->obj->dynseg.pltgot.addr + iter->wordsize * iter->index;
