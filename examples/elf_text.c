fe01888d9dd2e4b4c5f4dfeb71fa72cc161a2210
ryan <ryan@bitlackeys.org>
2019-01-30 17:32:36 -0800

    updated elf_text.c

---
+++ examples/elf_text.c
@@ -0,0 +1,39 @@
+/*
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <search.h>
+#include <sys/time.h>
+#include "../include/libelfmaster.h"
+
+int main(int argc, char **argv)
+{
+	elfobj_t obj;
+	elf_error_t error;
+	struct elf_symbol symbol;
+
+	if (argc < 2) {
+		printf("Usage: %s <binary>\n", argv[0]);
+		exit(EXIT_SUCCESS);
+	}
+	if (elf_open_object(argv[1], &obj,
+	    ELF_LOAD_F_SMART|ELF_LOAD_F_FORENSICS, &error) == false) {
+		fprintf(stderr, "%s\n", elf_error_msg(&error));
+		return -1;
+	}
+	if (elf_flags(&obj, ELF_SCOP_F) == true) {
+		printf("SCOP binary\n");
+	}
+	ssize_t ret = elf_scop_text_filesz(&obj);
+	printf("ret: %zu\n", ret);
+	if (ret > 0) {
+		printf("Total text segment size: %lu bytes\n", ret);
+	}
+	printf("elf_executable_text_base: %lx offset: %lx\n", elf_executable_text_base(&obj),
+	    elf_executable_text_offset(&obj));
+	elf_close_object(&obj);
+}
