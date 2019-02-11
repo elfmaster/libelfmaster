eed710f99312cf3b4509a56068ea8d1149f29059
ryan <ryan@bitlackeys.org>
2019-02-11 15:11:46 -0800

    added scount.c

---
+++ examples/scount.c
@@ -0,0 +1,30 @@
+/*
+ * Prints the dynamic symbol names with the corresponding PLT address
+ * Same as plt_dump2, but uses the plt iterator.
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <search.h>
+#include <sys/time.h>
+#include "../include/libelfmaster.h"
+
+int main(int argc, char **argv)
+{
+	elfobj_t obj;
+	elf_error_t error;
+
+	if (argc < 2) {
+		printf("Usage: %s <binary>\n", argv[0]);
+		exit(EXIT_SUCCESS);
+	}
+	if (elf_open_object(argv[1], &obj, ELF_LOAD_F_FORENSICS, &error) == false) {
+		fprintf(stderr, "%s\n", elf_error_msg(&error));
+		return -1;
+	}
+	printf("Number of sections: %zu\n", elf_section_count(&obj));
+	return 0;
+}
